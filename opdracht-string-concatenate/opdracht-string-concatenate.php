04a0f8d8bcbe11cf56a78549ca3199c2cc0fd8ee
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2017-11-21 02:39:52 +0100

    test

---
+++ opdracht-string-concatenate/opdracht-string-concatenate.php
@@ -0,0 +1,42 @@
+<?php
+
+	$voornaam = "Arthur";
+	$achternaam = "Van Passel";
+
+	$volledigeNaam	=	$voornaam . $achternaam;
+
+    $naamLengte	=	strlen( $volledigeNaam );
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht string concatenate</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+    		<h1>Opdracht string concatenate: deel 1</h1>
+
+    		<ul>
+                <li>Plaats je voornaam en familienaam in afzonderlijke variabelen</li>
+                <li>Concateneer beide variabelen en stop ze in een nieuwe variabele <code>$volledigeNaam</code></li>
+                <li>Druk de variabele af</li>
+                <li>Druk vervolgens het aantal karakters in <code>$volledigeNaam</code> af op een nieuwe lijn</li>
+    		</ul>
+        
+            <p><?php echo $volledigeNaam ?></p>
+            
+            <p><?= $naamLengte ?></p>
+
+        </section>
+
+    </body>
+</html>
