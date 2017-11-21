e1865f5f55fe8340356284da7de57c7fce4364ab
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2017-11-21 02:50:47 +0100

    opdracht-comments

---
+++ opdracht-comments/comments-deel1.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+		Arthur Van Passel
+		arthur.vanpassel@student.kdg.be
+	*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oplossing comments: deel1</title>
+	</head>
+	<body>
+		<h1>Oplossing comments: deel1</h1>
+		<?php echo 'Arthur Van Passel' ?>
+	</body>
+</html>
---
+++ opdracht-comments/comments-deel2.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+		Arthur Van Passel
+		arthur.vanpassel@student.kdg.be
+	*/
+
+
+echo '<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Oplossing comments: deel2</title>
+			</head>
+			<body>
+				<h1>Oplossing comments: deel2</h1>
+				Arthur Van Passel
+			</body>
+		</html>';
+?>
