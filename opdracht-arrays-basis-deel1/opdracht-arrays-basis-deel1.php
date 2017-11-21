69a1e12c4b7343c9dbee738573d8527bd7613dd8
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2017-11-21 04:14:29 +0100

    Arrays

---
+++ opdracht-arrays-basis-deel1/opdracht-arrays-basis-deel1.php
@@ -0,0 +1,34 @@
+<?php
+
+   // Eerste manier
+	$dieren =  array('kat', 
+                     'hond', 
+                     'schaap', 
+                     'slang');
+
+   // Tweede manier
+   $dieren[]   =  'kat';
+   $dieren[]   =  'hond';
+   $dieren[]   =  'schaap';
+   $dieren[]   =  'slang';
+
+   // Multidimensionale array
+   $voertuigen =  array(
+       'landvoertuigen'  => array('jeep', 'formule 1'),
+       'watervoertuig'   => array('hoovercraft', 'waterfiets'),
+       'luchtvoertuig'   => array('Gulfstream G6'),); 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Odracht arrays basis: deel1</title>
+	</head>
+	<body>
+
+      <h1>Opdracht arrays basis: deel1</h1>
+
+      <pre><?php var_dump ( $dieren )  ?></pre>
+		<pre><?php var_dump ( $voertuigen )  ?></pre>
+	</body>
+</html>
