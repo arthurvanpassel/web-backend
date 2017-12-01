1b0c0da434da6a55f0a934d6bfeddc15e01b473f
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2017-12-01 10:31:07 +0100

    functies recursive deel 2

--- opdracht-functies-recursive/opdracht-functies-recursive.php
+++ opdracht-functies-recursive/opdracht-functies-recursive.php
@@ -21,6 +21,31 @@
 
     $totaalKapitaal = berekenKapitaal(100000,10,10);
 
+
+    //DEEL 2
+
+    function berekenKapitaal2($data)
+    {
+        if ($data['counter'] <= $data['duur'])
+        {
+            $renteprijs = floor($data['kapitaal'] * ($data['rente']*0.01));
+            $data['kapitaal'] += $renteprijs;
+            $data['tijdverloop'][$data['counter']] = "Hans' kapitaal na ".$data['counter']." jaar is ".$data['kapitaal']." euro.";
+            
+            $data['counter']++;
+            
+            return berekenKapitaal2($data);
+        }
+        else
+            return $data;
+    }
+
+    $totaalKapitaal2 = berekenKapitaal2(array('kapitaal'=>100000,
+                                      'rente'=>8,
+                                      'duur'=>10,
+                                      'counter'=>1,
+                                      'tijdverloop'=>array()));
+
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +58,12 @@
     <h1>Opdracht functies recursive deel 1</h1>
     
     <?php foreach($totaalKapitaal as $value): ?>
+        <p><?= $value ?></p>
+    <?php endforeach ?>
+    
+    <h1>Deel 2</h1>
+    
+    <?php foreach($totaalKapitaal2['tijdverloop'] as $value): ?>
         <p><?= $value ?></p>
     <?php endforeach ?>
     
