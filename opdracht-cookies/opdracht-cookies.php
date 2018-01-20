aed1e1b6e5d48395a5e849e4c1e863e0eb76027d
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2018-01-20 11:51:27 +0100

    opdracht cookies deel 4

--- opdracht-cookies/opdracht-cookies.php
+++ opdracht-cookies/opdracht-cookies.php
@@ -4,16 +4,13 @@
 	if (isset($_GET['logout'])) {
 	
 		setcookie('authenticated','', time() - 3600 );
-			
 		header('location: opdracht-cookies.php');
 	}
 
     //LOG IN DATA
-    $logindata = explode(",",file_get_contents('deel-1.txt'));
-    // => logindata[0] = 'jan'
-    // => logindata[1] = 'paswoord01'
+    $logindata = JSON_decode(file_get_contents('deel-1.txt'), true);
     $isAuthenticated = false;
-    $message = '';
+    $message = false;
 
         
 
@@ -21,22 +18,29 @@
 	if ( isset( $_COOKIE['authenticated'] ) ) 
 	{
 		$isAuthenticated	=	true;
+        $id = $_COOKIE['authenticated'];
 	}
     else
     {
         //ON PASSWORD SUBMIT
         if (isset($_POST['submit']) ) 
         {
-
-            if ( $_POST[ 'password' ] == $logindata[ 1 ] || $_POST[ 'username' ] == $logindata[ 0 ] )
+            foreach ( $logindata as $id => $user )
             {
-                setcookie( 'authenticated', TRUE, time() + 10 );
-                header( 'location: opdracht-cookies.php' );
-            } 
-            else 
-            {
-
-                $message = 'Gebruikersnaam en/of paswoord niet correct. Probeer opnieuw. ' . $_POST['username'] . $_POST['password'] . ' - ' . $logindata[0] . $logindata[1];
+                if ( $_POST[ 'username' ] == $user['username'] && $_POST[ 'password' ] == $user['password'] )
+                {
+                    if ($_POST['remember'])
+                    {
+                        setcookie( 'authenticated', $id, time() + 2592000 );   //60s*60m*24u*30d
+                    }
+                    else
+                    {
+                        setcookie( 'authenticated', $id, time() + 10 );
+                    }
+                    header( 'location: opdracht-cookies.php' );
+                    break;
+                }
+                    $message = 'Gebruikersnaam en/of paswoord niet correct. Probeer opnieuw. ' . $_POST['username'] . $_POST['password'] . ' - ' . $user["username"] . $user['password'];
             }
         }
     }
@@ -50,16 +54,14 @@
     <title>Opdracht Cookies</title>
 </head>
 <body>
-    <h1>Opdracht cookies - deel 1</h1>
-    <h1>Inloggen</h1>
-    
+    <h1>Opdracht cookies</h1>
     <?php if ( $isAuthenticated ):	?>
-        <p>U bent ingelogd.</p>
+        <h1>Dashboard</h1>
+        <p>Hallo <?= $logindata[$id]['username'] ?>, fijn dat je er weer bij bent!</p>
         <p><a href="opdracht-cookies.php? logout=true">Uitloggen</a></p>
     <?php else: ?>
-        <?php if ( $message ): ?>
-            <p><?php echo $message ?></p>
-        <?php endif ?>
+        <h1>Inloggen</h1>
+        <p><?= $message ?></p>
         <form action="opdracht-cookies.php" method="post">
             <ul>
                 <li>
@@ -69,6 +71,10 @@
                 <li>
                     <label for="password">paswoord</label>
                     <input type="text" id="password" name="password">
+                </li>
+                <li>
+                    <input type="checkbox" id="remember" name="remember">
+                    <label for="remember">Mij onthouden</label>
                 </li>
             </ul>
             <input type="submit" name="submit">
